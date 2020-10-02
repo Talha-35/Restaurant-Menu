ca93ff4361be275a42c2a73db5f868a20a283c94
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-10-02 23:55:19 +0200

    menu-project uploaded

---
+++ index.js
@@ -0,0 +1,132 @@
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+      id: 10,
+      title: "bison steak",
+      category: "dinner",
+      price: 22.99,
+      img: "./images/item-10.jpeg",
+      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+  ];
+
+  const menuItems = document.querySelector('.section-center');
+  const buttons = document.querySelector('.btn-container');
+  window.addEventListener("DOMContentLoaded",function(){
+    allMenu(menu);
+    displayCatagories();
+  })
+  function allMenu(menulist) {
+    let menuElement = ''
+    menulist.forEach(item=>{
+    menuElement += `<article class="menu-item">
+          <img src=${item.img} alt="menu item" class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item['price']}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+          </div>
+        </article>`;
+    })
+    menuItems.innerHTML = menuElement
+  }
+  function displayCatagories() {
+   let buttonElement = `<button type="button" class="filter-btn" data-id="all">all</button>`
+   let menuArray = []
+   menu.forEach(item =>{
+     if(menuArray.indexOf(item.category) == -1){
+       menuArray.push(item.category)
+      }
+    })
+    menuArray.forEach(item=>{
+      buttonElement += `<button type="button" class="filter-btn" data-id="${item}">${item}</button>`
+    })
+    buttons.innerHTML = buttonElement
+    const filterButton = document.querySelectorAll('.filter-btn');
+    filterButton.forEach(item =>{
+      item.addEventListener('click',function(e){
+        const selectedCatogory = e.target.getAttribute('data-id');
+        const filteredName = menu.filter( (element) =>
+          element.category === selectedCatogory
+        )
+        if(selectedCatogory == 'all'){
+          allMenu(menu)
+        }else{
+          allMenu(filteredName)
+        }
+      })
+    })
+  }
